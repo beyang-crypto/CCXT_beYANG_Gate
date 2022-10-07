2ba2c953c4fec4bf2b10c463192d4481750f20f9
TestingAccMar <114166594+TestingAccMar@users.noreply.github.com>
2022-10-07 18:23:07 +0400

    Delete gate/spotAndMargin/v4 directory

--- gate/spotAndMargin/v4/ws/process.go
+++
@@ -1,9 +0,0 @@
-package spotandmargin
-
-func (b *GateWS) processTicker(symbol string, data Tickers) {
-	b.Emit(ChannelTicker, symbol, data)
-}
-
-func (b *GateWS) processWalletBalance(symbol string, data WalletBalance) {
-	b.Emit(ChannelBalances, data)
-}
